5b7827701dd6441d20a074c4b31c724629d97508
Yuya ONUMA <yohnuma@gion-xy.net>
2016-10-18 08:47:44 +0900

    change  URL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://pyhorse.github.io'
+SITEURL = 'https://pyhorse.gunma.jp'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
